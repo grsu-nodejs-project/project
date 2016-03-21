524ab9565e8194f9c1c84391558c09881f4f963b
vviital <vviital@gmail.com>
2016-03-21 08:46:39 +0300

    add mongodb to project

---
+++ intgame/lib/questionsSchema.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var questionSchema = new Schema({
+	link : {type: String, required: true, unique: true},
+	question : {type: String, required: true}
+});
+
+var Question = mongoose.model('Question', questionSchema);
+
+module.exports = Question;
