6b2ffb61ec27238cf77e3e28ac17713028515c6f
vviital <vviital@gmail.com>
2016-03-28 14:09:21 +0300

    improve mocha tests

--- intgame/test/questionsTest.js
+++ intgame/test/questionsTest.js
@@ -1,19 +1,13 @@
-//how I can start app this
-var app = require('../app');
 var request = require('supertest')('http://localhost:3000');
 var assert = require('chai').assert;
 
 describe('Questions routes test', function() {
-  it('module app exist', function() {
-    assert.isDefined(app, 'app module dosn\'t define in this scope');
-  });
   it('get /questions', function(done) {
     request
     .get('/questions')
     .expect(200)
     .expect('Content-type', 'text/html; charset=utf-8')
     .end(function(err, res) {
-      // why we return done(err) ???
       if (err) {
         return done(err);
       }
@@ -25,7 +19,7 @@ describe('Questions routes test', function() {
     .post('/questions')
     .send({link: 'http://db.chgk.info/tour/turn13'})
     .expect(200)
-    .expect('Content-type', 'text/html; charset=utf-8')
+    .expect('Content-type', 'application/json; charset=utf-8')
     .end(function(err, res) {
       if (err) {
         return done(err);
