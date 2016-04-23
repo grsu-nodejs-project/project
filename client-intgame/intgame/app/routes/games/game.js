946d6744261533ff46834c60559eeff88542068a
vviital <vviital@gmail.com>
2016-04-23 21:41:21 +0300

    working checker answer

--- client-intgame/intgame/app/routes/games/game.js
+++ client-intgame/intgame/app/routes/games/game.js
@@ -5,13 +5,42 @@ export default Ember.Route.extend(AuthenticatedRouteMixin,{
   model(params) {
     return this.store.find('game', params.game_id)
   },
+  checkAnswer(playerAnswer, correctAnswer) {
+    if (playerAnswer == "" || playerAnswer == undefined) return false;
+    let length1 = playerAnswer.length;
+    let length2 = correctAnswer.length;
+    let dp = new Array();
+    for(let i = 0; i <= length1; ++i) {
+      dp[i] = new Array();
+      for(let j = 0; j <= length2; ++j)
+        dp[i][j] = 1000000000;
+    }
+    for(let i = 0; i < length1; ++i) dp[i][0] = i;
+    for(let j = 0; j < length2; ++j) dp[0][j] = j;
+    for(let i = 1; i <= length1; ++i) {
+      for(let j = 1; j <= length2; ++j) {
+        if (playerAnswer.charAt(i - 1) === correctAnswer.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1];
+        else dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
+      }
+    }
+    let value = dp[length1][length2];
+    console.log(value);
+    if (value * 3 >= length2) return false;
+    return true;
+  },
+
   actions: {
     answer(param) {
       let controller = this.controllerFor('games.game');
       let nextQuestion = controller.get('questionsNumber') + 1;
       let allQuestion = this.currentModel.get('questionsNumber');
+      let playerAnswer = param.answer;
+      console.log(playerAnswer);
+      let correctAnswer = this.currentModel.get('questions')[nextQuestion - 1]['answer'];
+      console.log(correctAnswer);
+      console.log(this.get('checkAnswer')(playerAnswer, correctAnswer));
       if (nextQuestion == allQuestion) {
-        console.log('end');
+        this.transitionTo('profile');
       } else {
         controller.set('questionsNumber', nextQuestion);
       }
--- intgame/lib/htmlToObjectConverter.js
+++ intgame/lib/htmlToObjectConverter.js
@@ -8,19 +8,27 @@ var convertHtmlToObject = function(html, className, callback) {
   $(className).each(function() {
     var obj = new Object();
     var context = this;
-    needFindClass.forEach(function(value) {
+    needFindClass.forEach(function(value, index) {
       var text = $(context).find($('.' + value)).parent().text();
+      if (index === 1) {
+        text = processAnswer(text);
+      }
       obj[value.toLowerCase()] = text.trim();
     });
     var image = $(context).find('img').attr('src');
     if (image) {
       obj.image = image;
     } else {
-      obj.image = "";
+      obj.image = '';
     }
     resultArray.push(obj);
   });
   callback(resultArray);
 };
+
+function processAnswer(answer) {
+  answer = answer.trim().substr(6);
+  return answer;
+}
 
 exports.convertHtmlToObject = convertHtmlToObject;
